f3026d7e532a4f1bbd0d077c33184a4bc39a450d
Unknown <unknown@unknown.com>
2024-07-07 21:09:40 +0300

    c00 eklendi

---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fturedi <fturedi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/05 14:54:05 by fturedi           #+#    #+#             */
+/*   Updated: 2024/02/05 21:27:23 by fturedi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	z;
+
+	z = 'z';
+	while (z >= 'a')
+	{
+		write(1, &z, 1);
+		z--;
+	}
+}
+
+
+int main()
+{
+	ft_print_reverse_alphabet();
+	return 0;
+}
